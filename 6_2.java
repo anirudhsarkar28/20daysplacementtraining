ba82cacb78043b5b8075511b4e3ab4ec1056c35f
Anirudh Sarkar <82276184+anirudhsarkar28@users.noreply.github.com>
2026-06-01 14:51:57 +0530

    Create 6_2.java

---
+++ 6_2.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Main {
+  public static void main(String[] args) {
+    for (int i=1; i<11; i=i+1){
+        int w = i * 7;
+        System.out.println("7 * " + (i) + " = " + (w) );
+    }
+  }
+}
